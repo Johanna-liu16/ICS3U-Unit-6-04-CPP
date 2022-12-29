7129a70847f01ce5766b90974cd8f8116271a254
Johanna Liu <johanna.liu@mths.ca>
2022-12-29 16:21:23 +0000

    finished

--- average.cpp
+++ average.cpp
@@ -8,20 +8,20 @@
 #include <array>
 #include <iostream>
 #include <random>
-#include <string>
+#include <cmath>  // for rounding
 
 template <int rows, int cols>
-double CalculateAverage(int (&passedIn2DList)[rows][cols]) {
+float CalculateAverage(int (&passedIn2DList)[rows][cols]) {
     // This function calculates the average
-    int average = 0;
-    int sum = 0;
-    double length = sizeof(passedIn2DList) / sizeof(passedIn2DList[0]);
+    float average = 0;
+    double sum = 0;
+    float length = sizeof(passedIn2DList) / sizeof(passedIn2DList[0]);
 
     for (size_t rowElement = 0; rowElement < rows; ++rowElement) {
         for (size_t columnElement = 0; columnElement < cols; ++columnElement)
             sum += passedIn2DList[rowElement][columnElement];
     }
-    average = sum / (rows * cols);
+    average = round(sum / (rows * cols) * 100) / 100;
 
     return average;
 }
@@ -30,18 +30,20 @@ int main() {
     // This function uses an array
 
     int aRandomNumber;
-    int totalAverage;
+    double totalAverage;
     const int rows = 3;
     const int columns = 5;
     std::string strRows;
     std::string strColumns;
-    int a2DArray [rows][columns];
+    int a2DArray[rows][columns];
 
-    srand(time(NULL));
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());
+    std::uniform_int_distribution<int> idist(1, 50);
 
     for (int rowElement = 0; rowElement < rows; rowElement++) {
         for (int columnElement = 0; columnElement < columns; columnElement++) {
-            aRandomNumber = (rand() % 10) + 1;
+            aRandomNumber = idist(rgen);
             a2DArray[rowElement][columnElement] = aRandomNumber;
             std::cout << aRandomNumber << ", ";
         }
@@ -50,7 +52,8 @@ int main() {
 
     // call function
     totalAverage = CalculateAverage(a2DArray);
-    std::cout << "The average of all the numbers is: " << totalAverage << std::endl;
+    std::cout << "The average of all the numbers is: " << totalAverage
+    << std::endl;
 
     std::cout << "\nDone." << std::endl;
 }
